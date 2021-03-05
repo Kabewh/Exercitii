7185a54968927b0bc2b58dc2f69864975c16cd8a
Kabewh <73640269+Kabewh@users.noreply.github.com>
2021-03-05 12:33:53 +0200

    Add files via upload
    cod zen

--- Source.c
+++ Source.c
@@ -1,91 +1,88 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-int main() {
-	
-	/*FULLY WORKING*/
-	char rasp[10];
-	printf("Simtiti durere in zona toracelui?\n");
-	scanf("%s", &rasp);
-	char * pch;
-	pch = strstr(rasp, "da");
 
-	while (rasp == pch) {
-		char rasp1[10];
-		printf("Tusiti?\n", rasp1);
-		scanf("%s", &rasp1);
-		char* pch1;
-		pch1 = strstr(rasp1, "da");
+#include <stdlib.h>
+float BMI(float weight, float height)
+{
+    float result = weight / height * 2;
+    return result;
+}
 
-		if (rasp1 == pch1) {
-			char rasp2[10];
-			printf("Ce fel de tuse aveti?\n", rasp2);
-			scanf("%s", &rasp2);
-			char* pch2;
-			pch2 = strstr(rasp2, "uscata");
-			if (rasp2 != pch2) {
-				printf("Atunci este posibil sa aveti dispnee\nDaca este dispnee medie aveti pneumonie\nDaca starea este grava aveti un enfizem pulmonar\n");
-			}
-			if (rasp2 == pch2) {
-				int rasp3;
-				printf("Ce temperatura aveti?\n");
-				scanf("%d", &rasp3);
-				if (rasp3 <= 38) {
-					printf("Aveti traheita");
-					break;
-				}
-				if (rasp3 > 38) {
-					printf("Aveti bronsita");
-					break;
-				}
-			}
-			break;
-		}
-		break;
-	}
-	
-	while (rasp != pch) {
-		char xrasp[10];
-		printf("Va doare in gat?\n");
-		scanf("%s", &xrasp);
-		char* xpch;
-		xpch = strstr(xrasp, "da");
-		if (xrasp == xpch) {
-			float xrasp1;
-			printf("Ce temperatura aveti?\n");
-			scanf("%f", &xrasp1);
-			if (xrasp1 <= 38.5) {
-				printf("Aveti rinita\n");
-			}
-			if (xrasp1 > 38.5) {
-				printf("Aveti amigdalita\n");
-			}
-		}
-		if (xrasp != xpch) {
-			char yrasp[10];
-			printf("Va doare capul?\n");
-			scanf("%s", &yrasp);
-			char* ypch;
-			ypch = strstr(yrasp, "da");
-			if (yrasp == ypch) {
-				float yrasp1;
-				printf("Ce temperatura aveti?\n");
-				scanf("%f", &yrasp1);
-				if (yrasp1 <= 37.5) {
-					printf("Aveti o migrena\n");
-				}
-				if (yrasp1 > 37.5) {
-					printf("Aveti gripa\n");
-				}
-			}
-
-		}
-		break;
-	}
+int main()
+{
+    int counter = 0;
+    int counter2 = 0;
+    int counter3 = 0;
+    int result_len;
 
 
+    float bmi_result;
+    float results_bmi[50];
+    float greutate_totata = 0;
+    float inaltime_totata = 0;
+    float bmi_totata = 0;
 
+    char* results_id[50][3];
+    char* results_name[50][50];
 
+    char* singleLine_arr[4];
+    char* singleLine[50];
+    char* str_token;
 
-	return 0;
+    FILE* fPointer;
+    fPointer = fopen("test.txt", "r");
+
+    while (!feof(fPointer))
+    {
+        fgets(singleLine, 50, fPointer);
+        str_token = strtok(singleLine, ",");
+
+        counter = 0;
+        while (str_token != NULL)
+        {
+            ////remove \n
+            str_token[strcspn(str_token, "\n")] = 0;
+            //// add the value to the array
+            singleLine_arr[counter++] = str_token;
+            str_token = strtok(NULL, ",");
+        }
+
+        bmi_result = BMI(atof(singleLine_arr[2]), atof(singleLine_arr[3]));
+        greutate_totata = greutate_totata + atof(singleLine_arr[3]);
+        inaltime_totata = inaltime_totata + atof(singleLine_arr[2]);
+
+        strcpy(results_id[counter2], singleLine_arr[0]);
+        strcpy(results_name[counter2], singleLine_arr[1]);
+        results_bmi[counter2] = bmi_result;
+        counter2++;
+
+    }
+
+    fclose(fPointer);
+
+    FILE* fPointer2;
+    fPointer2 = fopen("result1.txt", "w+");
+    result_len = sizeof(results_bmi) / sizeof(results_bmi[0]);
+
+    for (int j = 0; j < result_len; j++)
+    {
+        if (results_bmi[j] > 0.01)
+        {
+
+            bmi_totata = results_bmi[0] + results_bmi[1] + results_bmi[2] + results_bmi[3] + results_bmi[4] + results_bmi[5];
+
+            fprintf(fPointer2, "Id este: %s, Name este: %s, BMI este: %f\n", results_id[j], results_name[j], results_bmi[j]);
+
+        }
+
+    }
+    fprintf(fPointer2, "%s\n", "----------------");
+    fprintf(fPointer2, "Medii greutate este: %f\n", greutate_totata / counter2);
+    fprintf(fPointer2, "Medii inaltime este: %f\n", inaltime_totata / counter2);
+    fprintf(fPointer2, "Medii BMI este: %f\n", bmi_totata / counter2);
+
+    fclose(fPointer2);
+    return 0;
+
 }
