750e2774c986f0b9f7ac7b80b3454222adb8ca71
Kabewh <73640269+Kabewh@users.noreply.github.com>
2021-03-08 13:21:25 +0200

    Add files via upload

--- program scai.c
+++ program scai.c
@@ -2,6 +2,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+typedef struct persoana
+{
+    int greutate;
+    float inaltime;
+    int id;
+    float bmi;
+}persoana;
+void setpers(persoana* pers, int id, float inaltime, int greutate, float bmi) 
+{
+    pers->id = id;
+    pers->inaltime = inaltime;
+    pers->greutate = greutate;
+    pers->bmi = bmi;
+}
+
 int main()
 {
     FILE* file1;
@@ -15,12 +30,11 @@ int main()
     float inaltime;
     float bmi;
     char x = "stop";
-    char* persoana[32];
-    
+    char* nume[32];
+    persoana* persoane[20];
     file1 = fopen("persoane.txt", "w"); 
     file3 = fopen("dateout.txt", "w");  
     printf("Meniu adaugare persoane. \n\n\n");
-    
     for (i = 0; i != x; ++i)
     {
         char rasp[5];
@@ -34,7 +48,7 @@ int main()
             nruser++;
             cid++;
             
-            printf("Introdu o noua persoana: "); scanf("%s", persoana);
+            printf("Introdu o noua persoana: "); scanf("%s", nume);
             printf("\n");
             printf("Id-ul tau este: %d\n", nruser); 
             printf("Introdu greutatea in kg: "); scanf("%f", &greutate);
@@ -43,6 +57,7 @@ int main()
             printf("\n");
             bmi = greutate / (inaltime * inaltime);
             printf("BMI-ul tau este: %f\n", bmi);
+            setpers(persoane[i], nruser, greutate, inaltime);
             if (bmi < 18.5) 
             {
                 printf("Risc ridicat; greutate prea mica.\n");
@@ -64,9 +79,9 @@ int main()
                 printf("Risc ridicat; Greutate prea mare\n");
             }
             // Salveaza datele intr-un fisier "persoane.txt" (input data)
-            fprintf(file1, "%s\tID: %d\tGreutate: %f\tInaltime: %f\n", persoana, nruser, greutate, inaltime);
+            fprintf(file1, "%s\tID: %d\tGreutate: %f\tInaltime: %f\n", nume, nruser, greutate, inaltime);
             // Salveaza  datele intr-un fisier "dateout.txt" (output data)
-            fprintf(file3, "ID: %d\tNume: %s\tBMI: %f\n", nruser, persoana, bmi);
+            fprintf(file3, "ID: %d\tNume: %s\tBMI: %f\n", nruser, nume, bmi);
         }
         if (rasp != ps)
         {
@@ -74,6 +89,7 @@ int main()
             break;
         }
     }
+    printf("%d", persoane[0]->id);
     fclose(file1);
     fclose(file3);
 
@@ -81,9 +97,7 @@ int main()
 
     // ========================================== //
     // Codu de jos ar trebui ca in momentu in care eu introduc id-ul unei persoane, el sa imi afiseze numele persoanei care corespunde cu id-ul introdus.
-    // Ideea e ca merge doar pentru ultima persoana introdusa, nu stiu cum sa fac sa accesez celelalte persoane introduse in meniul de adaugare persoane de mai sus
-    // Ca dupa sa pot face treaba asta. |
-    //                                  V
+    // Ideea e ca merge doar pentru ultima persoana introdusa, nu stiu cum sa fac sa stochez celelalte persoane introduse in meniul de adaugare persoane de mai sus
     char rasp1[5];
     printf("Doresti sa cauti o persoana dupa id?\n");
     scanf("%s", &rasp1);
@@ -97,7 +111,7 @@ int main()
             scanf("%d", &nruser);
             if (nruser == cid)
             {
-                printf("Id-ul %d apartine utilizatorului %s\n", nruser, persoana);
+                printf("Id-ul %d apartine utilizatorului %s\n", nruser, nume);
             }
             else if (nruser != cid) 
             {
